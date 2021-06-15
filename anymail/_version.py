b33a7e763f990ac0bdb491e02074fccf2267146b
medmunds <medmunds@gmail.com>
2021-06-15 14:40:52 -0700

    Release 8.4.0.dev2

--- anymail/_version.py
+++ anymail/_version.py
@@ -1,3 +1,3 @@
-VERSION = (8, 4, 0, 'dev1')
+VERSION = (8, 4, 0, 'dev2')
 __version__ = '.'.join([str(x) for x in VERSION])  # major.minor.patch or major.minor.devN
 __minor_version__ = '.'.join([str(x) for x in VERSION[:2]])  # Sphinx's X.Y "version"
